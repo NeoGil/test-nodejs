7c7c9e84d76c100d8cf7d48d493e758a250f2914
Denis Solovev <neogil28@gmail.com>
2023-06-27 23:56:51 +0300

    Added module: top-page

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { TopPageModule } from './top-page/top-page.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, TopPageModule],
   controllers: [AppController],
   providers: [AppService],
 })
