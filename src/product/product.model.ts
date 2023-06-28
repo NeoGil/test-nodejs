9cf59b7077aca320cfcfd7498ddee715eea4aa8b
Denis Solovev <neogil28@gmail.com>
2023-06-28 16:59:34 +0300

    Tabulation has been fixed models have been modified

--- src/product/product.model.ts
+++ src/product/product.model.ts
@@ -1,16 +1,16 @@
 export class ProductModel {
-  image: string;
-  title: string;
-  price: number;
-  oldprice: number;
-  credit: number;
-  calculatedRaiting: number;
-  discription: string;
-  advantages: string;
-  disAdvantages: string;
-  categories: string[];
-  tags: string;
-  characterisitics: {
-    [key: string]: string;
-  };
+    image: string;
+    title: string;
+    price: number;
+    oldprice: number;
+    credit: number;
+    calculatedRaiting: number;
+    description: string;
+    advantages: string;
+    disAdvantages: string;
+    categories: string[];
+    tags: string;
+    characterisitics: {
+        [key: string]: string;
+    };
 }
