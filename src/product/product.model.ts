53872cfb119535072993b592033b5687ee81d276
Denis Solovev <neogil28@gmail.com>
2023-07-02 17:52:12 +0300

    Updated models for MongoDB

--- src/product/product.model.ts
+++ src/product/product.model.ts
@@ -1,17 +1,54 @@
-export class ProductModel {
-    _id: string;
+/* eslint-disable indent */
+import { prop } from '@typegoose/typegoose';
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+
+class ProductCharacteristic {
+    @prop()
+    name: string;
+
+    @prop()
+    value: string;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface ProductModel extends Base {}
+export class ProductModel extends TimeStamps {
+    @prop()
     image: string;
+
+    @prop()
     title: string;
+
+    @prop()
+    link: string;
+
+    @prop()
+    initialRating: number;
+
+    @prop()
     price: number;
-    oldprice: number;
+
+    @prop()
+    oldPrice?: number;
+
+    @prop()
     credit: number;
-    calculatedRaiting: number;
+
+    @prop()
     description: string;
+
+    @prop()
     advantages: string;
-    disAdvantages: string;
+
+    @prop()
+    disAdvantages?: string;
+
+    @prop({ type: () => [String] })
     categories: string[];
-    tags: string;
-    characterisitics: {
-        [key: string]: string;
-    };
+
+    @prop({ type: () => [String] })
+    tags: string[];
+
+    @prop({ type: () => [ProductCharacteristic], _id: false })
+    characteristics: ProductCharacteristic[];
 }
